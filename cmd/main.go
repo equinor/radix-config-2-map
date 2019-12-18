057b4e87b879cb5f6a789714c5f8d3ba8e93c769
ingeknudsen <iknu@equinor.com>
2019-12-18 10:51:52 +0100

    Ra 1150 clone config in separate job (#1)
    * First piece of implementation
    
    * Renamed repository
    
    * Prepare for release
    
    * Updated dependencies

---
+++ cmd/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/equinor/radix-clone-config/pkg/configmap"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var (
+	// Command line flags
+	namespace     string
+	configMapName string
+	file          string
+)
+
+func main() {
+	rootCmd := &cobra.Command{
+		Use:   "clone-config",
+		Short: "Radix clone config",
+		Run:   root}
+
+	pf := rootCmd.PersistentFlags()
+
+	pf.StringVar(&namespace, "namespace", "",
+		"namespace configmap should be applied to")
+	pf.StringVar(&configMapName, "configmap-name", "",
+		"name to give to configmap")
+	pf.StringVar(&file, "file", "",
+		"absolute path to file")
+
+	cobra.MarkFlagRequired(pf, "namespace")
+	cobra.MarkFlagRequired(pf, "configmap-name")
+	cobra.MarkFlagRequired(pf, "file")
+
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func root(cmd *cobra.Command, args []string) {
+	log.Info("Running root command")
+	log.Infof("Namespace: %s", namespace)
+	log.Infof("Config map: %s", configMapName)
+	log.Infof("File: %s", file)
+
+	configmap.CreateFromFile(namespace, configMapName, file)
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/equinor/radix-config-2-map
+
+go 1.13
+
+require (
+	github.com/imdario/mergo v0.3.8 // indirect
+	github.com/sirupsen/logrus v1.4.2
+	github.com/spf13/cobra v0.0.5
+	golang.org/x/oauth2 v0.0.0-20191202225959-858c2ad4c8b6 // indirect
+	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
+	k8s.io/api v0.17.0
+	k8s.io/apimachinery v0.17.0
+	k8s.io/client-go v11.0.0+incompatible
+	k8s.io/utils v0.0.0-20191218082557-f07c713de883 // indirect
+)
