ee640ef1284f1c2ed427907b29b7a37141e7d9d7
Joakim Hagen <Joakim.Hagen@no.experis.com>
2020-05-27 12:32:05 +0000

    yaml filename candidates

--- pkg/configmap/configmap_util.go
+++ pkg/configmap/configmap_util.go
@@ -1,7 +1,9 @@
 package configmap
 
 import (
+	"fmt"
 	"io/ioutil"
+	"strings"
 
 	"github.com/equinor/radix-config-2-map/pkg/kubernetes"
 	corev1 "k8s.io/api/core/v1"
@@ -10,12 +12,19 @@ import (
 
 // CreateFromFile Creates a configmap by name from file
 func CreateFromFile(namespace, name, file string) error {
-	client := kubernetes.GetKubernetesClient()
-	content, err := ioutil.ReadFile(file)
+	var content []byte
+	var err error
+	for _, filename := range filenameCandidates(file) {
+		content, err = ioutil.ReadFile(filename)
+		if err == nil {
+			break
+		}
+	}
 	if err != nil {
-		return err
+		return fmt.Errorf("Could not find or read config yaml file \"%s\"", file)
 	}
 
+	client := kubernetes.GetKubernetesClient()
 	_, err = client.CoreV1().ConfigMaps(namespace).Create(
 		&corev1.ConfigMap{
 			ObjectMeta: metav1.ObjectMeta{
@@ -32,4 +41,18 @@ func CreateFromFile(namespace, name, file string) error {
 	}
 
 	return nil
+}
+
+func filenameCandidates(filename string) []string {
+	if strings.HasSuffix(filename, ".yaml") {
+		filename = filename[:len(filename)-5]
+	} else if strings.HasSuffix(filename, ".yml") {
+		filename = filename[:len(filename)-4]
+	}
+
+	return []string{
+		filename + ".yaml",
+		filename + ".yml",
+		filename,
+	}
 }
